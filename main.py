7671517bbffb47e1b6509350336e6aa520ad68a6
Rosepate <rozypatel942403@gmail.com>
2025-05-22 03:48:22 -0400

    feat: completed Person A tasks – cleaned, encoded, and split dataset

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import seaborn as sns
+
+penguins = sns.load_dataset("penguins")
+
+print(penguins.head())
+
+print(penguins.isnull().sum())
+
+df = penguins.dropna()
+print(df.isnull().sum())
+print("\nTotal missing values:", df.isnull().sum().sum())
+print(df.info())
+
+X = df.drop("species", axis=1)
+y = df["species"]
+
+X = pd.get_dummies(X)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+# Show results
+print("Dataset split")
+print("X_train shape:", X_train.shape)
+print("X_test shape :", X_test.shape)
+print("y_train shape:", y_train.shape)
+print("y_test shape :", y_test.shape)
