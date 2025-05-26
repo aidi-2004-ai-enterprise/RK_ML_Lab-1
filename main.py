6ddf504c3031278ba9f2a296830d1334ced1762d
Rosepate <rozypatel942403@gmail.com>
2025-05-26 14:25:24 -0400

    Save local changes before pull

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ X = pd.get_dummies(X)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-# Show results
 print("Dataset split")
 print("X_train shape:", X_train.shape)
 print("X_test shape :", X_test.shape)
